add7f74176815951de5a940d54c13531e5fca5a8
Giau <giaunv@gmail.com>
2015-03-11 07:49:14 +0700

    A Cascade of Cookies

--- cookiecrunch/GameViewController.swift
+++ cookiecrunch/GameViewController.swift
@@ -71,6 +71,10 @@ class GameViewController: UIViewController{
     
     func handleMatches(){
         let chains = level.removeMatches()
+        if chains.count == 0{
+            beginNextTurn()
+            return
+        }
         
         scene.animateMatchedCookies(chains){
             let columns = self.level.fillHoles()
@@ -79,9 +83,14 @@ class GameViewController: UIViewController{
                 let columns = self.level.topUpCookies()
                 
                 self.scene.animateNewCookies(columns){
-                    self.view.userInteractionEnabled = true
+                    self.handleMatches()
                 }
             }
         }
+    }
+    
+    func beginNextTurn(){
+        level.detectPossibleSwaps()
+        view.userInteractionEnabled = true
     }
 }
